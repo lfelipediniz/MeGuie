dd5761492ad381bf6567e19f92f9c7ffafd64fac
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-11-14 18:54:02 -0300

    feat: update side rav

--- src/app/[locale]/components/SideBar.tsx
+++ src/app/[locale]/components/SideBar.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import React, { useState } from "react";
-import {
-  Drawer,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  IconButton,
-} from "@mui/material";
-import InboxIcon from "@mui/icons-material/Inbox";
-import MailIcon from "@mui/icons-material/Mail";
-import MenuIcon from "@mui/icons-material/Menu";
+import { Drawer, IconButton, Tooltip } from "@mui/material";
+import { FaAngleLeft, FaAngleRight, FaHome, FaCalendarAlt, FaStar } from "react-icons/fa";
+import { IoAccessibility } from "react-icons/io5";
+import { MdContrast, MdTextFields } from "react-icons/md";
+import { GiHand } from "react-icons/gi";
 
 const Sidebar: React.FC = () => {
   const [expanded, setExpanded] = useState(false);
@@ -23,23 +17,10 @@ const Sidebar: React.FC = () => {
   return (
     <div className="z-50">
       <div className="hidden md:block">
-        {/* Botão para abrir/fechar a sidebar */}
-        <IconButton
-          onClick={toggleSidebar}
-          sx={{
-            position: "fixed",
-            top: 16,
-            left: 16,
-            zIndex: 1300,
-          }}
-        >
-        </IconButton>
-
         <Drawer
           variant="permanent"
           open={expanded}
           sx={{
-            position: "fixed",
             width: expanded ? 240 : 60,
             flexShrink: 0,
             transition: "width 0.3s",
@@ -52,28 +33,98 @@ const Sidebar: React.FC = () => {
             },
           }}
         >
-          <List>
-            <ListItem component="li" onClick={toggleSidebar}>
-              <ListItemIcon>
-                <MenuIcon />
-              </ListItemIcon>
-              {expanded && <ListItemText primary="Menu" />}
-            </ListItem>
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "10px" }}>
+            <Tooltip title="Acessibilidade">
+              <IconButton onClick={toggleSidebar} size="medium">
+                {expanded ? (
+                  <span style={{ display: "flex", alignItems: "center" }}>
+                    <IoAccessibility /> &nbsp; Acessibilidade
+                  </span>
+                ) : (
+                  <IoAccessibility />
+                )}
+              </IconButton>
+            </Tooltip>
 
-            <ListItem component="li">
-              <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon>
-              {expanded && <ListItemText primary="Inbox" />}
-            </ListItem>
+            <Tooltip title="Alto Contraste">
+              <IconButton size="medium">
+                {expanded ? (
+                  <span style={{ display: "flex", alignItems: "center" }}>
+                    <MdContrast /> &nbsp; Alto Contraste
+                  </span>
+                ) : (
+                  <MdContrast />
+                )}
+              </IconButton>
+            </Tooltip>
 
-            <ListItem component="li">
-              <ListItemIcon>
-                <MailIcon />
-              </ListItemIcon>
-              {expanded && <ListItemText primary="Mail" />}
-            </ListItem>
-          </List>
+            <Tooltip title="Libras">
+              <IconButton size="medium">
+                {expanded ? (
+                  <span style={{ display: "flex", alignItems: "center" }}>
+                    <GiHand /> &nbsp; Libras
+                  </span>
+                ) : (
+                  <GiHand />
+                )}
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Texto">
+              <IconButton size="medium">
+                {expanded ? (
+                  <span style={{ display: "flex", alignItems: "center" }}>
+                    <MdTextFields /> &nbsp; Texto
+                  </span>
+                ) : (
+                  <MdTextFields />
+                )}
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Tela Principal">
+              <IconButton size="medium">
+                {expanded ? (
+                  <span style={{ display: "flex", alignItems: "center" }}>
+                    <FaHome /> &nbsp; Tela Principal
+                  </span>
+                ) : (
+                  <FaHome />
+                )}
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Calendário">
+              <IconButton size="medium">
+                {expanded ? (
+                  <span style={{ display: "flex", alignItems: "center" }}>
+                    <FaCalendarAlt /> &nbsp; Calendário
+                  </span>
+                ) : (
+                  <FaCalendarAlt />
+                )}
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Roadmaps Favoritos">
+              <IconButton size="medium">
+                {expanded ? (
+                  <span style={{ display: "flex", alignItems: "center" }}>
+                    <FaStar /> &nbsp; Favoritos
+                  </span>
+                ) : (
+                  <FaStar />
+                )}
+              </IconButton>
+            </Tooltip>
+          </div>
+
+          {/* Botão para abrir/fechar a sidebar */}
+          <div style={{ position: "absolute", bottom: "30px", width: "100%", display: "flex", justifyContent: "center" }}>
+            <IconButton onClick={toggleSidebar} size="medium">
+              {expanded ? <FaAngleLeft /> : <FaAngleRight />}
+            </IconButton>
+          </div>
         </Drawer>
       </div>
     </div>
