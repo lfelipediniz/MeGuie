6a784a8af4f2bd7185d32bf60f640678820dc82b
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-11-14 11:08:33 -0300

    feat: add pages title

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -6,8 +6,8 @@ import { FC } from "react";
 import LogoIcon from "../../icons/logo";
 import LangSwitcher from "./LangSwitcher";
 import ThemeSwitch from "./ThemeSwitch";
-import PageList from "./PageList";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"; // imports updated icons
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import pageNamesData from "@/data/br/pagesTitle.json";
 
 interface Props {
   locale: string;
@@ -15,21 +15,15 @@ interface Props {
 
 export const Header: FC<Props> = ({ locale }) => {
   const t = useTranslations("");
-  const pathname = usePathname(); // Captura a URL atual
+  const pathname = usePathname(); // capturando a url atual
   const [menuOpen, setMenuOpen] = useState(false); // state to control the menu
   const menuRef = useRef<HTMLDivElement>(null);
 
-  const eventPages = [
-    { name: "Understanding DL", path: "/pages/events/understandingDL" },
-    // { name: "DataDay", path: "/pages/events/dataDay" },
-  ];
-
-  const isEventPage = eventPages.some((page) => pathname === page.path);
+  // Define o nome da página atual com base no JSON
+  const atualPageName = pageNamesData[pathname as keyof typeof pageNamesData] || "Página Não Encontrada";
 
   const getLinkClass = (path: string) => {
-    return pathname === path || (path === "/pages/events" && isEventPage)
-      ? "text-data-purple"
-      : "";
+    return pathname === path ? "text-data-purple font-bold" : "";
   };
 
   // Close the menu when clicking outside
@@ -52,13 +46,12 @@ export const Header: FC<Props> = ({ locale }) => {
   }, [menuOpen]);
 
   return (
-    <div
-      className="fixed top-0 left-0 right-0 z-50 bg-background shadow-md transition-all duration-300"
-    >
+    <div className="fixed top-0 left-0 right-0 z-50 bg-background shadow-md transition-all duration-300">
       <div className="mx-auto flex max-w-screen-2xl items-center justify-between p-5">
+        {/* Logo */}
         <Link
           lang={locale}
-          href={`/`}
+          href={"/"}
           className={getLinkClass("/")}
           onClick={() => setMenuOpen(false)}
         >
@@ -69,7 +62,11 @@ export const Header: FC<Props> = ({ locale }) => {
           </div>
         </Link>
 
-        {/* hamburger menu button for mobile */}
+        <div className="flex-1 flex justify-center md:justify-center text-center font-bold">
+          {atualPageName}
+        </div>
+
+        {/* Botão do menu hambúrguer para mobile */}
         <div className="flex md:hidden">
           <button onClick={() => setMenuOpen(!menuOpen)}>
             {menuOpen ? (
@@ -80,94 +77,20 @@ export const Header: FC<Props> = ({ locale }) => {
           </button>
         </div>
 
-        {/* menu for mobile devices */}
+        {/* Menu para dispositivos móveis */}
         <div
           ref={menuRef}
-          className={`mobile-menu md:hidden absolute left-0 right-0 top-full mt-2 bg-white shadow-md rounded-md ${
-            menuOpen ? "open" : ""
+          className={`md:hidden absolute right-0 top-full mt-2 bg-white shadow-md rounded-md ${
+            menuOpen ? "block" : "hidden"
           } z-50`}
         >
-          <div className="flex flex-col items-center justify-center">
-            <Link
-              lang={locale}
-              href={`/pages/fronts`}
-              className={`${getLinkClass("/pages/fronts")} block`}
-              onClick={() => setMenuOpen(false)}
-            >
-              {t("Header.Fronts")}
-            </Link>
-
-            {/* about page link */}
-            <Link
-              lang={locale}
-              href={`/pages/about`}
-              className={`${getLinkClass("/pages/about")} block`}
-              onClick={() => setMenuOpen(false)}
-            >
-              {t("Header.About")}
-            </Link>
-            {
-              <PageList
-                locale={locale}
-                pages={eventPages}
-                pageListName={t("Header.Events")}
-                active={isEventPage} // Pass the active state
-              />
-            }
-            <Link
-              lang={locale}
-              href={`/pages/contact`}
-              className={`${getLinkClass("/pages/contact")} block`}
-              onClick={() => setMenuOpen(false)}
-            >
-              {t("Header.Contact")}
-            </Link>
-            <div className="flex flex-row items-center space-x-4">
-              <ThemeSwitch />
-              <LangSwitcher locale={locale} />
-            </div>
+          <div className="flex flex-col items-center justify-center p-4">
+            <ThemeSwitch />
+            <LangSwitcher locale={locale} />
           </div>
         </div>
 
-        {/* default menu for larger screens */}
-        <div className="hidden md:flex flex-1 flex-col items-center gap-10 text-center font-bold">
-          <div className="flex gap-10 items-center">
-            <Link
-              lang={locale}
-              href={`/pages/fronts`}
-              className={getLinkClass("/pages/fronts")}
-              onClick={() => setMenuOpen(false)}
-            >
-              {t("Header.Fronts")}
-            </Link>
-            <Link
-              lang={locale}
-              href={`/pages/about`}
-              className={`${getLinkClass("/pages/about")} block`}
-              onClick={() => setMenuOpen(false)}
-            >
-              {t("Header.About")}
-            </Link>
-            {
-              <PageList
-                locale={locale}
-                pages={eventPages}
-                pageListName={t("Header.Events")}
-                active={isEventPage} // Pass the active state
-              />
-            }
-            <Link
-              lang={locale}
-              href={`/pages/contact`}
-              className={getLinkClass("/pages/contact")}
-              onClick={() => setMenuOpen(false)}
-            >
-              {t("Header.Contact")}
-            </Link>
-          </div>
-        </div>
-
-        {/* theme and language switcher for desktop */}
+        {/* Theme e language switcher para desktop */}
         <div className="hidden md:flex flex-row items-center gap-3">
           <ThemeSwitch />
           <LangSwitcher locale={locale} />
