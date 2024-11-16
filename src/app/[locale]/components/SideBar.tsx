13a76fb193552405680121ec0949d7145d364176
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-11-16 18:41:11 -0300

    feat: update sidebar

--- src/app/[locale]/components/SideBar.tsx
+++ src/app/[locale]/components/SideBar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import { Drawer, IconButton, Tooltip, Divider } from "@mui/material";
 import {
@@ -45,178 +45,189 @@ const Sidebar: React.FC = () => {
 
   return (
     <div className="h-md:block hidden">
-    <div className="z-50 md:block hidden">
-      <Drawer
-        variant="permanent"
-        open={expanded}
-        sx={{
-          width: expanded ? 240 : 60,
-          flexShrink: 0,
-          transition: "width 0.3s",
-          "& .MuiDrawer-paper": {
+      <div className="z-50 md:block hidden">
+        <Drawer
+          variant="permanent"
+          open={expanded}
+          sx={{
             width: expanded ? 240 : 60,
-            overflowX: "hidden",
-            position: "fixed",
-            zIndex: 1200,
+            flexShrink: 0,
             transition: "width 0.3s",
-          },
-        }}
-      >
-        {/* Logo do App */}
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: expanded ? "flex-start" : "center",
-            padding: "10px 16px",
-            marginBottom: "5px",
-            transition: "all 0.3s",
+            "& .MuiDrawer-paper": {
+              width: expanded ? 240 : 60,
+              overflowX: "hidden",
+              position: "fixed",
+              zIndex: 1200,
+              transition: "width 0.3s",
+            },
           }}
-          onClick={() => router.push("/")}
-          className="cursor-pointer"
         >
+          {/* Logo do App */}
           <div
             style={{
-              color: "var(--contrast-bt-nav)",
-              fontSize: "1.8rem",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: expanded ? "flex-start" : "center",
+              padding: "10px 16px",
+              marginBottom: "5px",
+              transition: "all 0.3s",
+            }}
+            onClick={() => router.push("/")}
+            className="cursor-pointer"
+          >
+            <div
+              style={{
+                color: "var(--contrast-bt-nav)",
+                fontSize: "1.8rem",
+                marginTop: "10px",
+              }}
+            >
+              <LogoIcon />
+            </div>
+            <span
+              style={{
+                marginLeft: expanded ? "15px" : "0",
+                opacity: expanded ? 1 : 0,
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                transition: "opacity 0.3s, margin-left 0.3s",
+                fontSize: "1.2rem",
+                color: "var(--contrast-bt-nav)",
+                fontWeight: "bold",
+                marginTop: "14px",
+              }}
+            >
+              MeGuie
+            </span>
+          </div>
+
+          <Divider style={{ margin: "10px 0" }} />
+
+          {/* Navegação */}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
               marginTop: "10px",
             }}
           >
-            <LogoIcon />
-          </div>
-          <span
-            style={{
-              marginLeft: expanded ? "15px" : "0",
-              opacity: expanded ? 1 : 0,
-              whiteSpace: "nowrap",
-              overflow: "hidden",
-              transition: "opacity 0.3s, margin-left 0.3s",
-              fontSize: "1.2rem",
-              color: "var(--contrast-bt-nav)",
-              fontWeight: "bold",
-              marginTop: "14px",
-            }}
-          >
-            MeGuie
-          </span>
-        </div>
-
-        <Divider style={{ margin: "10px 0" }} />
-
-        {/* Navegação */}
-        <div style={{ display: "flex", flexDirection: "column", marginTop: "10px" }}>
-          {navItems.map(({ icon, label, path }, index) => (
-            <Tooltip title={!expanded ? label : ""} key={index}>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "flex-start",
-                  padding: "10px 16px",
-                  transition: "all 0.3s",
-                  cursor: "pointer",
-                }}
-                onClick={() => router.push(path as any)}
-              >
+            {navItems.map(({ icon, label, path }, index) => (
+              <Tooltip title={!expanded ? label : ""} key={index}>
                 <div
                   style={{
-                    color: pathname === path ? "var(--action)" : "var(--contrast-bt-nav)",
-                    fontSize: "1.5rem",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "flex-start",
+                    padding: "10px 16px",
+                    transition: "all 0.3s",
+                    cursor: "pointer",
                   }}
+                  onClick={() => router.push(path as any)}
                 >
-                  {icon}
+                  <div
+                    style={{
+                      color:
+                        pathname === path
+                          ? "var(--action)"
+                          : "var(--contrast-bt-nav)",
+                      fontSize: "1.5rem",
+                    }}
+                  >
+                    {icon}
+                  </div>
+                  <span
+                    style={{
+                      marginLeft: "15px",
+                      opacity: expanded ? 1 : 0,
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      transition: "opacity 0.3s",
+                      fontSize: "1.1rem",
+                      color:
+                        pathname === path
+                          ? "var(--action)"
+                          : "var(--contrast-bt-nav)",
+                    }}
+                  >
+                    {label}
+                  </span>
                 </div>
-                <span
-                  style={{
-                    marginLeft: "15px",
-                    opacity: expanded ? 1 : 0,
-                    whiteSpace: "nowrap",
-                    overflow: "hidden",
-                    transition: "opacity 0.3s",
-                    fontSize: "1.1rem",
-                    color: pathname === path ? "var(--action)" : "var(--contrast-bt-nav)",
-                  }}
-                >
-                  {label}
-                </span>
-              </div>
-            </Tooltip>
-          ))}
-        </div>
+              </Tooltip>
+            ))}
+          </div>
 
-        <Divider style={{ margin: "10px 0" }} />
+          <Divider style={{ margin: "10px 0" }} />
 
-     
-        <div
-          style={{
-            position: "absolute",
-            bottom: "30px",
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <IconButton
-            onClick={toggleSidebar}
-            size="medium"
-            style={{ color: "var(--contrast-bt-nav)" }}
-          >
-            {expanded ? <FaAngleLeft /> : <FaAngleRight />}
-          </IconButton>
-        </div>
-
-           {/* Seção de Perfil */}
-           <div
-          style={{
-            position: "absolute",
-            bottom: "80px",
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <img
-            src="https://thispersondoesnotexist.com/"
-            alt="Foto de Fulano de Tal"
+          <div
             style={{
-              width: "45px",
-              height: "45px",
-              borderRadius: "10px",
-              marginLeft: "6px",
-              objectFit: "cover",
-              marginRight: expanded ? "10px" : "0",
-              transition: "margin-right 0.3s",
-            }}
-          />
-          <span
-            style={{
-              flexGrow: 1,
-              opacity: expanded ? 1 : 0,
-              whiteSpace: "nowrap",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              transition: "opacity 0.3s",
-              fontSize: "1rem",
-              color: "var(--contrast-bt-nav)",
+              position: "absolute",
+              bottom: "30px",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
             }}
           >
-            Fulano de Tal
-          </span>
-          <IconButton
-            onClick={handleLogout}
-            size="small"
+            <IconButton
+              onClick={toggleSidebar}
+              size="medium"
+              style={{ color: "var(--contrast-bt-nav)" }}
+            >
+              {expanded ? <FaAngleLeft /> : <FaAngleRight />}
+            </IconButton>
+          </div>
+
+          {/* Seção de Perfil */}
+          <div
             style={{
-              color: "var(--contrast-bt-nav)",
-              marginLeft: "10px",
+              position: "absolute",
+              bottom: "80px",
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
             }}
           >
-            <FaSignOutAlt />
-          </IconButton>
-        </div>
-
-        
-      </Drawer>
-    </div>
+            <img
+              src="https://thispersondoesnotexist.com/"
+              alt="Foto de Fulano de Tal"
+              style={{
+                width: "45px",
+                height: "45px",
+                borderRadius: "10px",
+                marginLeft: "6px",
+                objectFit: "cover",
+                marginRight: expanded ? "10px" : "0",
+                transition: "margin-right 0.3s",
+              }}
+            />
+            <span
+              style={{
+                flexGrow: 1,
+                opacity: expanded ? 1 : 0,
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                transition: "opacity 0.3s",
+                fontSize: "1rem",
+                color: "var(--contrast-bt-nav)",
+                fontWeight: "bold",
+              }}
+            >
+              Fulano de Tal
+            </span>
+            <IconButton
+              onClick={handleLogout}
+              size="small"
+              style={{
+                color: "var(--contrast-bt-nav)",
+                marginLeft: "8px",
+                marginRight: "8px",
+              }}
+            >
+              <FaSignOutAlt color="var(--red)" size={20}/>
+            </IconButton>
+          </div>
+        </Drawer>
+      </div>
     </div>
   );
 };
