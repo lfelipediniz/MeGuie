88dde6be2b01d1ac10e9b3ae76102c98ad9643d2
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-11-16 17:12:26 -0300

    feat: add perfil on sidebar

--- src/app/[locale]/components/SideBar.tsx
+++ src/app/[locale]/components/SideBar.tsx
@@ -1,23 +1,19 @@
-"use client";
-
+"use client"
 import React, { useState } from "react";
-import { Drawer, IconButton, Tooltip, Menu, MenuItem, Divider } from "@mui/material";
+import { Drawer, IconButton, Tooltip, Divider } from "@mui/material";
 import {
   FaAngleLeft,
   FaAngleRight,
   FaHome,
   FaCalendarAlt,
   FaStar,
-  FaSignLanguage,
-  FaTextHeight,
+  FaSignOutAlt,
 } from "react-icons/fa";
-import { IoAccessibility, IoContrastSharp } from "react-icons/io5";
 import LogoIcon from "@/src/app/icons/logo";
 import { usePathname, useRouter } from "@/src/navigation";
 
 const Sidebar: React.FC = () => {
   const [expanded, setExpanded] = useState(false);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const pathname = usePathname();
   const router = useRouter();
 
@@ -25,12 +21,8 @@ const Sidebar: React.FC = () => {
     setExpanded(!expanded);
   };
 
-  const handleAccessibilityClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleAccessibilityClose = () => {
-    setAnchorEl(null);
+  const handleLogout = () => {
+    router.push("/"); // Redireciona para a página principal
   };
 
   const navItems: { icon: JSX.Element; label: string; path: string }[] = [
@@ -53,22 +45,22 @@ const Sidebar: React.FC = () => {
 
   return (
     <div className="z-50 md:block hidden">
-    <Drawer
-      variant="permanent"
-      open={expanded}
-      sx={{
-        width: expanded ? 240 : 60,
-        flexShrink: 0,
-        transition: "width 0.3s",
-        "& .MuiDrawer-paper": {
+      <Drawer
+        variant="permanent"
+        open={expanded}
+        sx={{
           width: expanded ? 240 : 60,
-          overflowX: "hidden",
-          position: "fixed",
-          zIndex: 1200,
+          flexShrink: 0,
           transition: "width 0.3s",
-        },
-      }}
-    >
+          "& .MuiDrawer-paper": {
+            width: expanded ? 240 : 60,
+            overflowX: "hidden",
+            position: "fixed",
+            zIndex: 1200,
+            transition: "width 0.3s",
+          },
+        }}
+      >
         {/* Logo do App */}
         <div
           style={{
@@ -101,7 +93,7 @@ const Sidebar: React.FC = () => {
               fontSize: "1.2rem",
               color: "var(--contrast-bt-nav)",
               fontWeight: "bold",
-              marginTop: "10px",
+              marginTop: "14px",
             }}
           >
             MeGuie
@@ -111,13 +103,7 @@ const Sidebar: React.FC = () => {
         <Divider style={{ margin: "10px 0" }} />
 
         {/* Navegação */}
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            marginTop: "10px",
-          }}
-        >
+        <div style={{ display: "flex", flexDirection: "column", marginTop: "10px" }}>
           {navItems.map(({ icon, label, path }, index) => (
             <Tooltip title={!expanded ? label : ""} key={index}>
               <div
@@ -159,69 +145,7 @@ const Sidebar: React.FC = () => {
 
         <Divider style={{ margin: "10px 0" }} />
 
-        {/* Botões de acessibilidade */}
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <Tooltip title={!expanded ? "Acessibilidade" : ""}>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "flex-start",
-                padding: "10px 16px",
-                transition: "all 0.3s",
-                cursor: "pointer",
-              }}
-              onClick={handleAccessibilityClick}
-            >
-              <div
-                style={{
-                  color: "var(--contrast-bt-nav)",
-                  fontSize: "1.5rem",
-                }}
-              >
-                <IoAccessibility />
-              </div>
-              <span
-                style={{
-                  marginLeft: "15px",
-                  opacity: expanded ? 1 : 0,
-                  whiteSpace: "nowrap",
-                  overflow: "hidden",
-                  transition: "opacity 0.3s",
-                  fontSize: "1.1rem",
-                }}
-              >
-                Acessibilidade
-              </span>
-            </div>
-          </Tooltip>
-        </div>
-
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleAccessibilityClose}
-          style={{ zIndex: 1300 }}
-        >
-          <MenuItem onClick={() => alert("Ativar Libras")}>
-            <FaSignLanguage style={{ marginRight: "10px" }} />
-            Ativar Libras
-          </MenuItem>
-          <MenuItem onClick={() => alert("Aumentar Texto")}>
-            <FaTextHeight style={{ marginRight: "10px" }} />
-            Aumentar Texto
-          </MenuItem>
-          <MenuItem onClick={() => alert("Ativar Alto Contraste")}>
-            <IoContrastSharp style={{ marginRight: "10px" }} />
-            Alto Contraste
-          </MenuItem>
-        </Menu>
-
+     
         <div
           style={{
             position: "absolute",
@@ -239,6 +163,57 @@ const Sidebar: React.FC = () => {
             {expanded ? <FaAngleLeft /> : <FaAngleRight />}
           </IconButton>
         </div>
+
+           {/* Seção de Perfil */}
+           <div
+          style={{
+            position: "absolute",
+            bottom: "80px",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src="https://thispersondoesnotexist.com/"
+            alt="Foto de Fulano de Tal"
+            style={{
+              width: "45px",
+              height: "45px",
+              borderRadius: "10px",
+              marginLeft: "6px",
+              objectFit: "cover",
+              marginRight: expanded ? "10px" : "0",
+              transition: "margin-right 0.3s",
+            }}
+          />
+          <span
+            style={{
+              flexGrow: 1,
+              opacity: expanded ? 1 : 0,
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              transition: "opacity 0.3s",
+              fontSize: "1rem",
+              color: "var(--contrast-bt-nav)",
+            }}
+          >
+            Fulano de Tal
+          </span>
+          <IconButton
+            onClick={handleLogout}
+            size="small"
+            style={{
+              color: "var(--contrast-bt-nav)",
+              marginLeft: "10px",
+            }}
+          >
+            <FaSignOutAlt />
+          </IconButton>
+        </div>
+
+        
       </Drawer>
     </div>
   );
