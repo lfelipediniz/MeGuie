ab3eff5282ae535babe1baff47d428acfbe9f7bd
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-11-26 15:02:17 -0300

    feat: update footer

--- src/app/[locale]/components/Footer.tsx
+++ src/app/[locale]/components/Footer.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { FaInstagram, FaFacebook, FaWhatsapp, FaEnvelope } from "react-icons/fa";
-
 import { useTranslations } from "next-intl";
 import { useTheme } from "next-themes";
 import { useEffect, useState, FC } from "react";
+import BinaryLogo from "@/src/app/icons/binaryLogo";
 
 interface Props {
   locale: string;
@@ -12,33 +12,33 @@ interface Props {
 export const Footer: FC<Props> = ({ locale }) => {
   const { resolvedTheme } = useTheme();
   const [logoSrc, setLogoSrc] = useState("/simpleDATAICON.png");
-  const t = useTranslations(""); // Para gerenciar as traduções
+
+  const t = useTranslations(""); 
 
   useEffect(() => {
     const updatedLogoSrc =
       resolvedTheme === "light"
-        ? "/simpleDATAICON-white.png" // Altere para o nome do arquivo da logo para whitemode
+        ? "/simpleDATAICON-white.png" 
         : "/simpleDATAICON.png";
     setLogoSrc(updatedLogoSrc);
   }, [resolvedTheme]);
 
   return (
-<footer className="bg-[var(--background-tertiary)] relative z-20 mt-auto w-full between-lg-md:p-54">
-
+    <footer className="bg-[var(--footer-bg)] text-[var(--background)] relative z-20 mt-auto w-full p-4 py-6 lg:py-8">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
             <a href={`/${locale}/`} className="flex items-center">
-              <img src={logoSrc} className="h-8 me-3" alt="MeGuie Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-primary">
+              <BinaryLogo width={35} height={35} /> &nbsp; 
+              <span className="self-center text-2xl font-semibold whitespace-nowrap">
                 MeGuie
               </span>
             </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-primary uppercase dark:text-primary">{t("Footer.Resources")}</h2>
-              <ul className="text-text-secondary dark:text-text-secondary font-medium">
+              <h2 className="mb-6 text-sm font-semibold uppercase text-[var(--background)]">{t("Footer.Resources")}</h2>
+              <ul className="font-medium text-[var(--background)]">
                 <li className="mb-4">
                   <a href={`/${locale}/pages/about`} className="hover:underline">{t("Footer.AboutUs")}</a>
                 </li>
@@ -48,8 +48,8 @@ export const Footer: FC<Props> = ({ locale }) => {
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-primary uppercase dark:text-primary">{t("Footer.FollowUs")}</h2>
-              <ul className="text-text-secondary dark:text-text-secondary font-medium">
+              <h2 className="mb-6 text-sm font-semibold uppercase text-[var(--background)]">{t("Footer.FollowUs")}</h2>
+              <ul className="font-medium text-[var(--background)]">
                 <li className="mb-4">
                   <a href="https://www.linkedin.com/school/meguie/" className="hover:underline">LinkedIn</a>
                 </li>
@@ -59,8 +59,8 @@ export const Footer: FC<Props> = ({ locale }) => {
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-primary uppercase dark:text-primary">{t("Footer.Legal")}</h2>
-              <ul className="text-text-secondary dark:text-text-secondary font-medium">
+              <h2 className="mb-6 text-sm font-semibold uppercase text-[var(--background)]">{t("Footer.Legal")}</h2>
+              <ul className="font-medium text-[var(--background)]">
                 <li className="mb-4">
                   <a href={`/${locale}/pages/events/understandingDL`} className="hover:underline">{t("Footer.Events")}</a>
                 </li>
@@ -71,27 +71,19 @@ export const Footer: FC<Props> = ({ locale }) => {
             </div>
           </div>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr className="my-6 border-[var(--footer-border-color)] sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-text-secondary sm:text-center dark:text-text-secondary">
+          <span className="text-sm sm:text-center text-[var(--background)]">
             © 2024 <a href={`/${locale}/`} className="hover:underline">MeGuie</a>. {t("Footer.AllRightsReserved")}
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
-            <a href="https://www.youtube.com/c/DataICMC" className="text-text-secondary hover:text-primary dark:hover:text-primary me-5">
+            <a href="https://www.youtube.com/c/DataICMC" className="hover:text-primary me-5">
               <FaInstagram className="w-4 h-4" />
               <span className="sr-only">YouTube</span>
             </a>
-            <a href="https://www.instagram.com/data.icmc/" className="text-text-secondary hover:text-primary dark:hover:text-primary me-5">
+            <a href="https://www.instagram.com/data.icmc/" className="hover:text-primary me-5">
               <FaFacebook className="w-4 h-4" />
               <span className="sr-only">Instagram</span>
-            </a>
-            <a href="https://www.linkedin.com/school/meguie/" className="text-text-secondary hover:text-primary dark:hover:text-primary me-5">
-              <FaWhatsapp className="w-4 h-4" />
-              <span className="sr-only">LinkedIn</span>
-            </a>
-            <a href="https://www.linkedin.com/school/meguie/" className="text-text-secondary hover:text-primary dark:hover:text-primary me-5">
-              <FaEnvelope className="w-4 h-4" />
-              <span className="sr-only">LinkedIn</span>
             </a>
           </div>
         </div>
@@ -99,5 +91,3 @@ export const Footer: FC<Props> = ({ locale }) => {
     </footer>
   );
 };
-
-export default Footer;
