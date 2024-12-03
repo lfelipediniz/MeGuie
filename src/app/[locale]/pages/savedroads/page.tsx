c5229099b35ca5678db86c32923f09844f6d4602
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-12-03 08:26:54 -0300

    feat: update search bar

--- src/app/[locale]/pages/savedroads/page.tsx
+++ src/app/[locale]/pages/savedroads/page.tsx
@@ -8,13 +8,13 @@ import LoadingOverlay from "../../components/LoadingOverlay";
 import { Footer } from "../../components/Footer";
 import RoadmapCard from "../../components/RoadmapCard";
 import { IoSearch } from "react-icons/io5";
-import { FaArrowLeft } from "react-icons/fa6";
 import TopicsModal from "../../components/TopicsModal";
+import SearchBar from "../../components/SearchBar";
 
 export default function SavedRoads() {
   const searchParams = useSearchParams();
   const router = useRouter();
-  const [showLoading, setShowLoading] = useState(false)
+  const [showLoading, setShowLoading] = useState(false);
   const [isFavorite, setIsFavorite] = useState(false);
   const [isTopicsModalOpen, setIsTopicsModalOpen] = useState(false);
   const [isMaterialsModalOpen, setIsMaterialsModalOpen] = useState(true);
@@ -24,7 +24,7 @@ export default function SavedRoads() {
   }
 
   function handleBack() {
-    router.back()
+    router.back();
   }
 
   const openTopicsModal = () => {
@@ -48,12 +48,7 @@ export default function SavedRoads() {
     openTopicsModal();
   }
 
-  const mathTopics = [
-    "Tópico 1",
-    "Tópico 2",
-    "Tópico 3",
-    "Tópico 4",
-  ];
+  const mathTopics = ["Tópico 1", "Tópico 2", "Tópico 3", "Tópico 4"];
 
   return (
     <div className="mt-16 p-4 md:p-8 bg-[var(--background-secondary)]">
@@ -64,22 +59,62 @@ export default function SavedRoads() {
       ) : (
         <div className="transition-opacity duration-500 opacity-100 mx-auto max-w-screen-2xl">
           <div className="w-full h-12 flex justify-between items-center gap-4 mb-4">
-            <button onClick={handleBack} className="h-12 w-12 flex justify-center items-center hover:bg-black/5 rounded-full transition duration-500">
-              <FaArrowLeft size={24} color={"var(--marine)"} />
-            </button>
-            <div className="w-full h-full rounded-xl shadow-xl bg-white px-4 flex items-center gap-4">
-              <IoSearch size={24} color={"var(--text-tertiary)"} />
-              <input type="text" placeholder="Pesquisar roadmap" className="placeholder-[var(--text-tertiary)] text-[var(--text-dark-blue)] text-lg outline-none w-full h-full" />
-            </div>
+
+          <SearchBar onSearch={(query) => console.log(query)} onBack={handleBack} />
+         
+          
           </div>
+          <br />
           <div className="w-full flex flex-col gap-4">
-            <h2 className="text-[var(--dark-blue)] text-xl md:text-2xl font-bold">Roadmaps</h2>
+            <h2 className="text-[var(--dark-blue)] text-xl md:text-2xl font-bold">
+              Roadmaps
+            </h2>
             <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-8">
-              <RoadmapCard image={"image_fisica.png"} title="Matemática" progress={40} isFavorite={isFavorite} toggleFavorite={toggleFavorite} topics={mathTopics} handleOpenTopics={handleOpenTopics} />
-              <RoadmapCard image={"image_fisica.png"} title="Matemática" progress={40} isFavorite={isFavorite} toggleFavorite={toggleFavorite} topics={mathTopics} handleOpenTopics={handleOpenTopics} />
-              <RoadmapCard image={"image_fisica.png"} title="Matemática" progress={40} isFavorite={isFavorite} toggleFavorite={toggleFavorite} topics={mathTopics} handleOpenTopics={handleOpenTopics} />
-              <RoadmapCard image={"image_fisica.png"} title="Matemática" progress={40} isFavorite={isFavorite} toggleFavorite={toggleFavorite} topics={mathTopics} handleOpenTopics={handleOpenTopics} />
-              <RoadmapCard image={"image_fisica.png"} title="Matemática" progress={40} isFavorite={isFavorite} toggleFavorite={toggleFavorite} topics={mathTopics} handleOpenTopics={handleOpenTopics} />
+              <RoadmapCard
+                image={"image_fisica.png"}
+                title="Matemática"
+                progress={40}
+                isFavorite={isFavorite}
+                toggleFavorite={toggleFavorite}
+                // topics={mathTopics}
+                handleOpenTopics={handleOpenTopics}
+              />
+              <RoadmapCard
+                image={"image_fisica.png"}
+                title="Matemática"
+                progress={40}
+                isFavorite={isFavorite}
+                toggleFavorite={toggleFavorite}
+                // topics={mathTopics}
+                handleOpenTopics={handleOpenTopics}
+              />
+              <RoadmapCard
+                image={"image_fisica.png"}
+                title="Matemática"
+                progress={40}
+                isFavorite={isFavorite}
+                toggleFavorite={toggleFavorite}
+                // topics={mathTopics}
+                handleOpenTopics={handleOpenTopics}
+              />
+              <RoadmapCard
+                image={"image_fisica.png"}
+                title="Matemática"
+                progress={40}
+                isFavorite={isFavorite}
+                toggleFavorite={toggleFavorite}
+                // topics={mathTopics}
+                handleOpenTopics={handleOpenTopics}
+              />
+              <RoadmapCard
+                image={"image_fisica.png"}
+                title="Matemática"
+                progress={40}
+                isFavorite={isFavorite}
+                toggleFavorite={toggleFavorite}
+                // topics={mathTopics}
+                handleOpenTopics={handleOpenTopics}
+              />
             </div>
           </div>
         </div>
